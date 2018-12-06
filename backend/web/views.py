15294dfc739ee331764137025e5806abb7bc5627
Carlos Leal <carlos.leal@ucla.edu>
2018-12-05 23:26:29 -0800

    Change link to candidate packet, can be changed from admin panel.

--- backend/web/views.py
+++ backend/web/views.py
@@ -3,7 +3,7 @@ import re
 
 from common import MyTemplateView
 from event.models import Event
-from main.models import Faculty, Officer, Settings
+from main.models import Faculty, Officer, Settings, Link
 
 about = MyTemplateView.as_view(template_name='about.html')
 awards = MyTemplateView.as_view(template_name='awards.html')
@@ -16,7 +16,6 @@ home = MyTemplateView.as_view(template_name='home.html',
                                   dropdown=True, end__gt=datetime.datetime.today) if event.event_type != Event.SOCIAL],
                                   'display': Settings.objects.display_tutoring()})
 programs = MyTemplateView.as_view(template_name='programs.html')
-requirements = MyTemplateView.as_view(template_name='requirements.html')
 tutoring = MyTemplateView.as_view(template_name='tutoring.html')
 sponsor = MyTemplateView.as_view(template_name='sponsor.html')
 donate = MyTemplateView.as_view(template_name='donate.html')
@@ -66,3 +65,5 @@ faculty = MyTemplateView.as_view(template_name='faculty.html',
                                  additional={'faculty': get_faculty_by_dept, 'facultyAdvisor': 'Ann R. Karagozian'})
 officers = MyTemplateView.as_view(template_name='officers.html', 
                                   additional={'term': Settings.objects.term(), 'positions': get_officers})
+requirements = MyTemplateView.as_view(template_name='requirements.html',
+                                      additional={'candidatePacketURL': Link.objects.filter(name='candidate_packet_url')})
