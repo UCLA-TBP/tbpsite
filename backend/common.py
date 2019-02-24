0414dc2aec23c66061c67abd92655500a688de76
Carter Wu <carter.wu@ucla.edu>
2019-02-24 12:45:24 -0800

    finish redesign

--- backend/common.py
+++ backend/common.py
@@ -6,7 +6,7 @@ from django.shortcuts import render as django_render
 from django.views.generic.base import TemplateView
 
 from event.models import Event
-from main.models import Settings, HousePoints
+from main.models import Settings, HousePoints, Link
 
 
 class MyTemplateView(TemplateView):
@@ -17,6 +17,12 @@ class MyTemplateView(TemplateView):
         context = {'user': self.request.user, 'next': self.request.path,  'events': Event.objects.filter(dropdown=True , term=Settings.objects.term()),
                    'houses': HousePoints.current.all(), 'eligibility_list': Settings.objects.get_eligibility_list().url}
         context.update(self.additional)
+        candidatePacketURLs = Link.objects.filter(name='candidate_packet_url')
+        if len(candidatePacketURLs):
+            context['candidatePacketURL'] = candidatePacketURLs[0].url
+        orientationPresentations = Link.objects.filter(name='orientation_presentation')
+        if len(orientationPresentations):
+            context['orientationPresentationURL'] = orientationPresentations[0].url
         return self.render_to_response(context)
 
 
@@ -25,4 +31,11 @@ def render(request, template_name, additional=None):
                   'houses': HousePoints.current.all(), 'eligibility_list': Settings.objects.get_eligibility_list().url}
     if additional is not None:
         dictionary.update(additional)
+
+    candidatePacketURLs = Link.objects.filter(name='candidate_packet_url')
+    if len(candidatePacketURLs):
+        dictionary['candidatePacketURL'] = candidatePacketURLs[0].url
+    orientationPresentations = Link.objects.filter(name='orientation_presentation')
+    if len(orientationPresentations):
+        dictionary['orientationPresentationURL'] = orientationPresentations[0].url
     return django_render(request, template_name, dictionary)
